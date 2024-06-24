072d7ba8744c9bdbe09d0b923c0530f1ce534c2f
Liidia Ilves <liidia_ilves@hotmail.com>
2024-06-25 00:45:34 +0300

    Add firebase authentication

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,12 +1,30 @@
 import { Link } from "react-router-dom";
+import { auth } from "../config/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 export const Navbar = () => {
+  // const [user, loading, error] = useAuthState(auth)
+  const [user] = useAuthState(auth);
+
   return (
-    <div>
-      <Link to="/">HOME</Link>
-      <Link to="/login" className="nav">
-        Login
-      </Link>
+    <div className="nav">
+      <div className="navEl navLink">
+        <Link to="/">HOME</Link>
+      </div>
+      <div className="navEl navLink">
+        <Link to="/login">Login</Link>
+      </div>
+      <div className="navEl navName">
+        <p>{user?.displayName}</p>
+      </div>
+      <div className="navEl">
+        <img
+          src={user?.photoURL || ""}
+          width="60"
+          height="60"
+          alt="profilePicture"
+        />
+      </div>
     </div>
   );
 };
